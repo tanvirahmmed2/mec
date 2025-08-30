25ff137ce290e476c40b015f4faee10bd0b9d557
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-30 23:06:24 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,69 +2,74 @@ import React from 'react'
 import logo from "../images/logo.png"
 import { Link } from 'react-router-dom'
 import { IoIosArrowDown } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
     return (
         <section className='w-full relative h-auto'>
-            <nav className='w-full h-28  flex flex-row items-center justify-around border-b-2 z-40'>
+            <nav className='w-full h-32  flex flex-col items-center justify-between border-b-2 z-40'>
+                <div className='w-full h-20 flex flex-row items-center justify-around'>
+                    <div className='w-auto h-20 flex flex-row items-center justify-center gap-3'>
+                        <a href="/"><img src={logo} alt="" className='h-16 w-16' /></a>
+                        <h1 className='w-auto h-16 text-2xl font-semibold flex items-center'>Mymensingh Engineering College</h1>
 
-                <div className='w-auto h-28 flex flex-row items-center justify-center gap-2'>
-                    <a href="/"><img src={logo} alt="" className='h-24' /></a>
-                    <div className='w-auto h-24 flex flex-col items-center justify-center gap-2 font-semibold text-2xl'>
-                        <h1 className=''>Mymensingh Engineering College</h1>
-                        <h1 className='opacity-65'>ময়মনসিংহ ইঞ্জিনিয়ারিং কলেজ</h1>
+
+                    </div>
+
+
+                    <div className=' w-auto h-12 flex flex-row items-center justify-cneter gap-2'>
+                        <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 ' to="/">Home</Link>
+                        <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 ' to="/student-account">Student apply</Link>
+                        <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 ' to="/alumni">Alumni</Link>
+                        <p className='w-auto h-12 flex items-center hover:border-b-2 px-4 cursor-pointer '><GiHamburgerMenu /></p>
                     </div>
 
                 </div>
-                <div className='w-auto h-28 flex flex-col items-end justify-center'>
 
-                    <div className=' w-auto h-14 flex flex-row items-center justify-cneter gap-2'>
-                        <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 ' to="/">Home</Link>
-                        <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 ' to="/student-account">Student apply</Link>
-                        <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 ' to="/alumni">Alumni</Link>
+                <div className=' w-full h-12 flex  flex-row items-center justify-center bg-red-800'>
+                    <div className='w-auto relative group'>
+                        <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 gap-2 text-white' to="/department">Department <IoIosArrowDown /></Link>
+                        <div className='w-80 rounded-md absolute hidden group-hover:flex p-2 flex-col items-start  bg-white '>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/ce">Department of Civil Engineering</Link>
+                            <Link className='w-auto px-2 block h-12 items-center hover:scale-105 transition duration-300' to="/eee">Department of Electrical & Electronic Engineering</Link>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/cse">Department of Computer Science & Engineering</Link>
+                        </div>
                     </div>
 
-                    <div className=' w-auto h-14 flex  flex-row items-center justify-center '>
-                        <div className='w-auto relative group'>
-                            <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 gap-2' to="/department">Department <IoIosArrowDown/></Link>
-                            <div className='w-80 rounded-md absolute hidden group-hover:flex p-2 flex-col items-start  bg-white '>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/ce">Department of Civil Engineering</Link>
-                                <Link className='w-auto px-2 block h-14 items-center hover:scale-105 transition duration-300' to="/eee">Department of Electrical & Electronic Engineering</Link>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/cse">Department of Computer Science & Engineering</Link>
-                            </div>
+                    <div className='w-auto relative group'>
+                        <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 gap-2 text-white' to="/department">Administration <IoIosArrowDown /></Link>
+                        <div className='w-80 rounded-md absolute hidden group-hover:flex p-2 flex-col items-start gap-2 bg-white '>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/faculty">Faculty</Link>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/officers">Officers</Link>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/staff">Staff</Link>
                         </div>
-                        
-                        <div className='w-auto relative group'>
-                            <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 gap-2' to="/department">Administration <IoIosArrowDown/></Link>
-                            <div className='w-80 rounded-md absolute hidden group-hover:flex p-2 flex-col items-start gap-2 bg-white '>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/faculty">Faculty</Link>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/officers">Officers</Link>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/staff">Staff</Link>
-                            </div>
-                        </div>
-                        <div className='w-auto relative group'>
-                            <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 gap-2' to="/department">Clubs <IoIosArrowDown/></Link>
-                            <div className='w-80 rounded-md absolute hidden group-hover:flex p-2 flex-col items-start gap-2 bg-white '>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/">Alpha Science Lab</Link>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/">MESL</Link>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/">MEC Debate Society</Link>
-                                <Link className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' to="/">MEC Films and Photography Society</Link>
-                            </div>
-                        </div>
-                        <div className='w-auto relative group'>
-                            <p className='w-auto h-14 flex items-center hover:border-b-2 px-4 gap-2 ' >Academics <IoIosArrowDown/></p>
-                            <div className='w-auto rounded-md absolute hidden group-hover:flex p-2 flex-col items-start  bg-white '>
-                                <a className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' href="https://cmc.du.ac.bd/result.php">Result</a>
-                                <a className='w-auto px-2 flex h-14 items-center hover:scale-105 transition duration-300' href="https://collegeadmission.eis.du.ac.bd/en">Admission</a>
-                            </div>
-                        </div>
-                        
-                        <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 ' to="/notices">Notices</Link>
-                        <Link className='w-auto h-14 flex items-center hover:border-b-2 px-4 ' to="/contacts">Contacts</Link>
                     </div>
-                   
+                    <div className='w-auto relative group'>
+                        <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 gap-2 text-white' to="/department">Clubs <IoIosArrowDown /></Link>
+                        <div className='w-80 rounded-md absolute hidden group-hover:flex p-2 flex-col items-start gap-2 bg-white '>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/">Alpha Science Lab</Link>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/">MESL</Link>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/">MEC Debate Society</Link>
+                            <Link className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' to="/">MEC Films and Photography Society</Link>
+                        </div>
+                    </div>
+                    <div className='w-auto relative group'>
+                        <p className='w-auto h-12 flex items-center hover:border-b-2 px-4 gap-2 text-white' >Academics <IoIosArrowDown /></p>
+                        <div className='w-auto rounded-md absolute hidden group-hover:flex p-2 flex-col items-start  bg-white '>
+                            <a className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' href="https://cmc.du.ac.bd/result.php">Result</a>
+                            <a className='w-auto px-2 flex h-12 items-center hover:scale-105 transition duration-300' href="https://collegeadmission.eis.du.ac.bd/en">Admission</a>
+                        </div>
+                    </div>
 
+                    <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 text-white' to="/notices">Notices</Link>
+                    <Link className='w-auto h-12 flex items-center hover:border-b-2 px-4 text-white' to="/contacts">Contacts</Link>
                 </div>
+
+
+
+
+
+
 
             </nav>
 
