2dae5fd3bdbfaf3dc6301e33e3d30db43594fafb
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-30 18:38:37 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from "../images/logo.png"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <section className='w-full relative h-auto'>
-            <nav className='w-full h-28  flex flex-row items-center justify-around bg-cyan-300'>
+            <nav className='w-full h-28  flex flex-row items-center justify-around border-b-2'>
 
                 <div className='w-auto h-28 flex flex-row items-center justify-center gap-2'>
                     <a href="/"><img src={logo} alt="" className='h-24' /></a>
@@ -15,12 +16,18 @@ const Navbar = () => {
 
                 </div>
                 <div className='w-auto h-28 bg-red-100 flex flex-col items-center justify-center'>
-                    <div className=''>
+
+                    <div className=' w-auto h-14 flex flex-row items-center justify-center gap-2'>
 
                     </div>
-                    <div>
 
+                    <div className=' w-auto h-14 flex flex-row items-center justify-center gap-2'>
+                        <Link>Home</Link>
+                        <Link>About</Link>
+                        <Link>Notices</Link>
+                        <Link>Contacts</Link>
                     </div>
+                   
 
                 </div>
 
