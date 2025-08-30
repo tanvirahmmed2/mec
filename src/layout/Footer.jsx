7895d276c9311ae6798f2d3f00ab971595c3fca8
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-30 23:16:23 +0600

    update

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -12,6 +12,8 @@ const Footer = () => {
     <div className='w-full h-auto bg-white flex flex-col items-center justify-center py-8'>
       <div className='w-full flex flex-col items-center justify-center gap-2'>
         <img src={logo} alt=""  className='h-20'/>
+        <h1 className='text-xl font-semibold'>Mymensingh Engineering College</h1>
+        <p>Established in 2007</p>
         <p>All Rights Reserved by Administration © {year}</p>
       </div>
     </div>
