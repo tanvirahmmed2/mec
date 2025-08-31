29bef2e4bc1c30d566d7bb92e28a001f7451ca53
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-01 02:19:36 +0600

    update

--- src/pages/Notices.jsx
+++ src/pages/Notices.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Notices = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='w-full flex flex-col items-center justify-center gap-6 lg:flex-row'>
+            <div className='w-full h-auto flex flex-col items-center justify-center'>
+                <p>Events</p>
+            </div>
+            <div className='w-full h-auto flex flex-col items-center justify-center'>
+                <p>Notices</p>
+            </div>
+        </section >
+    )
 }
 
 export default Notices
