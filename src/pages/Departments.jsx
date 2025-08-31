e6d9b31c101c17582630f0a4cae4374574a71b04
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-31 10:14:10 +0600

    update

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import deptDatas from "../Data/department"
 
 const Departments = () => {
-  return (
-    <section className='w-full h-auto flex flex-col items-center justify-center gap-6'>
-        <h1>Our Departments</h1>
-        <div></div>
-    </section>
-  )
+    return (
+        <section className='w-full h-auto flex flex-col items-center justify-center gap-6'>
+            <h1>Our Departments</h1>
+            <div>
+
+                <div>
+
+                </div>
+                <div className='w-full h-auto flex flex-wrap justify-center gap-6'>
+                    
+
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Departments
