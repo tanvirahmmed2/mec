817050860c44d711bdf90febc493442b4a5b90b3
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-31 11:04:52 +0600

    update

---
+++ src/Data/Teachers.js
@@ -0,0 +1,209 @@
+
+const Teachers =[
+    {
+      "id": 1,
+      "name": "অধ্যাপক ড. প্রকৌ. মোঃ মিজানুর রহমান",
+      "designation": "Professor (CE) & Principal (Acting)",
+      "department": "CE",
+      "email": "mec.ac.bd@gmail.com",
+      "mobile": [
+        "+8801911938473",
+        "+8801558450900"
+      ],
+      "office_phone": "+8802223376777",
+      "home_phone": "+8802223376776"
+    },
+    {
+      "id": 2,
+      "name": "আবদুর রউফ",
+      "designation": "Associate Professor (CSE) & Academic In-charge",
+      "department": "CSE",
+      "email": "roufmsc@gmail.com",
+      "mobile": [
+        "+8801928228338"
+      ]
+    },
+    {
+      "id": 3,
+      "name": "মোঃ রফিকুল ইসলাম",
+      "designation": "Associate Professor (Mathematics) & Head of Department (Non-Tech)",
+      "department": "Mathematics",
+      "email": "rafiqulislampro7878@gmail.com",
+      "mobile": [
+        "+8801724082658"
+      ],
+      "office_phone": "+8802223376779"
+    },
+    {
+      "id": 4,
+      "name": "মোঃ সালাহ উদ্দিন",
+      "designation": "Assistant Professor (EEE)",
+      "department": "EEE",
+      "email": "sumeceee@mec.ac.bd",
+      "mobile": [
+        "+8801737242628"
+      ]
+    },
+    {
+      "id": 5,
+      "name": "তাহিয়া রাব্বি",
+      "designation": "Assistant Professor & Head of Department (CE)",
+      "department": "CE",
+      "email": "tahia.rabbee.ce@mec.ac.bd",
+      "mobile": [
+        "+8801726200011"
+      ],
+      "office_phone": "+8802223376781"
+    },
+    {
+      "id": 6,
+      "name": "মুহাম্মদ আতিকুল হাসান",
+      "designation": "Assistant Professor (CE)",
+      "department": "CE",
+      "email": "atiqulh18@gmail.com",
+      "mobile": [
+        "+8801731254937"
+      ]
+    },
+    {
+      "id": 7,
+      "name": "মোঃ নূরুজ্জামান",
+      "designation": "Assistant Professor (CE)",
+      "department": "CE",
+      "email": "mnahid.cuet@mec.ac.bd",
+      "mobile": [
+        "+8801723264910"
+      ]
+    },
+    {
+      "id": 8,
+      "name": "অজিফাতুন জান্নাত আঁখি",
+      "designation": "Assistant Professor (CSE)",
+      "department": "CSE",
+      "email": "ozifa_cse@yahoo.com",
+      "mobile": [
+        "+8801911601126"
+      ]
+    },
+    {
+      "id": 9,
+      "name": "সাবরিনা সাত্তার সেতু",
+      "designation": "Lecturer (CSE)",
+      "department": "CSE",
+      "email": "setu151154756@gmail.com",
+      "mobile": [
+        "+8801953699248"
+      ]
+    },
+    {
+      "id": 10,
+      "name": "মোঃ মাহবুবুল ইমাম মজুমদার",
+      "designation": "Lecturer (Non-Tech/Physics)",
+      "department": "Non-Tech/Physics",
+      "email": "link2mahbub@gmail.com",
+      "mobile": [
+        "+8801635625069"
+      ]
+    },
+    {
+      "id": 11,
+      "name": "জেনিয়া সুলতানা",
+      "designation": "Lecturer (CE)",
+      "department": "CE",
+      "email": "jenia@mec.ac.bd",
+      "mobile": [
+        "+8801521528354"
+      ]
+    },
+    {
+      "id": 12,
+      "name": "সুশান্ত দেব নাথ",
+      "designation": "Lecturer (EEE)",
+      "department": "EEE",
+      "email": "susanta.eee.cuet@gmail.com",
+      "mobile": [
+        "+8801622691698"
+      ]
+    },
+    {
+      "id": 13,
+      "name": "উৎপল গোস্বামী",
+      "designation": "Workshop/Laboratory In-charge",
+      "department": "Workshop/Laboratory",
+      "email": "utpalgoswami08@gmail.com",
+      "mobile": [
+        "+8801750962746"
+      ]
+    },
+    {
+      "id": 14,
+      "name": "আব্দুল ওয়াহিদ",
+      "designation": "Lecturer (EEE) (Attached)",
+      "department": "EEE",
+      "email": "wahed.eee8421@mec.ac.bd",
+      "mobile": [
+        "+8801721343362"
+      ]
+    },
+    {
+      "id": 15,
+      "name": "মোহাম্মদ সাকিবুল আমিন",
+      "designation": "Lecturer (Social Science) Part-time",
+      "department": "Social Science",
+      "mobile": [
+        "+8801919842142"
+      ]
+    },
+    {
+      "id": 16,
+      "name": "মুয়াজ বিন আলী",
+      "designation": "Lecturer (CE) Part-time",
+      "department": "CE",
+      "email": "muazbinali100@gmail.com",
+      "mobile": [
+        "+8801871752467"
+      ]
+    },
+    {
+      "id": 17,
+      "name": "মোঃ মিজান কবির শুভ",
+      "designation": "Lecturer (EEE) Part-time",
+      "department": "EEE",
+      "email": "mkabirshuvo@gmail.com",
+      "mobile": [
+        "01622922469"
+      ]
+    },
+    {
+      "id": 18,
+      "name": "ইয়াছিন ইদমাম ইমন",
+      "designation": "Lecturer (CE) Part-time",
+      "department": "CE",
+      "email": "edmam.ce@gmail.com",
+      "mobile": [
+        "+8801638203847"
+      ]
+    },
+    {
+      "id": 19,
+      "name": "Md. Fokrul Islam",
+      "designation": "Lecturer (CSE) Part-time",
+      "department": "CSE",
+      "email": "fokrulcse30@gmail.com",
+      "mobile": [
+        "01521786010"
+      ]
+    },
+    {
+      "id": 20,
+      "name": "A.K.M Mahfuzur Rahman",
+      "designation": "Lecturer (CSE) Part-time",
+      "department": "CSE",
+      "email": "mahfuzking555@gmail.com",
+      "mobile": [
+        "01791261135"
+      ]
+    }
+  ]
+
+export default Teachers
