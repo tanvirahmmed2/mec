3b4ae92ebb45be5c37c55490822d5e86d891210c
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-01 02:10:55 +0600

    update

--- src/pages/Administration.jsx
+++ src/pages/Administration.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import Faculty from './Faculty'
+import { CreateContext } from '../context/CreateContext'
 
 const Administration = () => {
+  const { faculty } = useContext(CreateContext)
+  const principle = faculty[0]
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center gap-6'>
 
       <h1 className="text-3xl font-semibold text-center">Principle</h1>
-
+      <div key={principle.dept} className='w-[400px]  h-auto flex flex-col gap-2 p-2 border-2 rounded-lg hover:shadow-lg'>
+        <img src={principle.image} alt="" className='w-[400px]' />
+        <Link className='font-semibold text-lg' to={`/faculty/${principle.name}`}>{principle.name}</Link>
+        <p className='italic'>{principle.designation}</p>
+        <p>{principle.department}</p>
+      </div>
       <h1 className="text-3xl font-semibold text-center">Faculties</h1>
-      <Faculty/>
-      
+      <Faculty />
+
     </div>
   )
 }
