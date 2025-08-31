c26030b208ad5d61faad7f5f0a68014c958012f3
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-31 10:55:29 +0600

    update

--- src/pages/Department.jsx
+++ src/pages/Department.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import deptDatas from '../Data/department'
+import Faculty from './Faculty';
 
 const Department = () => {
       const { title } = useParams();
@@ -18,8 +19,13 @@ const Department = () => {
   const department = filteredDept[0]
   return (
     <section className='w-full h-auto flex flex-col items-center justify-center gap-6'>
-        <h1 className='text-3xl font-semibold'>{department.name}</h1>
-        <div></div>
+        <h1 className='text-3xl font-semibold text-center'>{department.name}</h1>
+        <p>{department.long_details}</p>
+        <div>
+            <p className='text-lg font-semibold'>Total students: {department.total_students}</p>
+            <p className='text-lg font-semibold'>Per year admission seats: {department.new_students_per_year}</p>
+        </div>
+        <Faculty/>
     </section>
   )
 }
