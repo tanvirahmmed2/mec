06ebeb2992eeb1b1eb759a92a6cd0d3eef1021c2
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-31 10:29:14 +0600

    update

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -5,20 +5,18 @@ import deptDatas from "../Data/department"
 const Departments = () => {
     return (
         <section className='w-full h-auto flex flex-col items-center justify-center gap-6'>
-            <h1>Our Departments</h1>
+            <h1 className='text-3xl font-semibold'>Our Departments</h1>
             <div>
 
-                <div>
 
-                </div>
                 <div className='w-full h-auto flex flex-wrap justify-center gap-6'>
                     {
                         deptDatas.map((dept) => {
                             const { name, title, short_details } = dept
-                            return <div key={title} className='w-[280px] shadow-sm rounded-lg p-1 overflow-hidden shadow-black flex flex-col items-start gap-2'>
+                            return <div key={title} className='w-[280px] shadow-sm rounded-lg p-1 overflow-hidden shadow-red-800 flex flex-col items-start gap-2 hover:scale-[1.03] transition duration-300 cursor-pointer'>
                                 <p>{short_details}</p>
-                                <h1>{name}</h1>
-                                <Link to={title}>Visit</Link>
+                                <h1 className='font-semibold text-red-800'>{name}</h1>
+                                <Link to={title} className='italic'>Visit</Link>
                             </div>
                         })
                     }
