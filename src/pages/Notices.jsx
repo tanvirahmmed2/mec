51d2422b520e451a406f778dc160cadb04d82317
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-01 10:28:34 +0600

    update

--- src/pages/Notices.jsx
+++ src/pages/Notices.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Notices = () => {
     return (
         <section className='w-full flex flex-col items-center justify-center gap-6 lg:flex-row'>
-            <div className='w-full h-auto flex flex-col items-center justify-center'>
+            <div className='w-full h-[300px] border-2 rounded-lg overflow-hidden flex flex-col items-center justify-center'>
                 <p>Events</p>
             </div>
-            <div className='w-full h-auto flex flex-col items-center justify-center'>
+            <div className='w-full h-[300px] border-2 rounded-lg overflow-hidden flex flex-col items-center justify-center'>
                 <p>Notices</p>
             </div>
         </section >
