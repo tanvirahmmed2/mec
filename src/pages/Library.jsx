ea1c5d716fe6fc80213c3142b1b8589a037aa722
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-01 17:54:48 +0600

    update

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -2,21 +2,21 @@ import React, { useContext } from 'react'
 import { CreateContext } from '../context/CreateContext';
 
 const Library = () => {
-  const {hoset_lib}= useContext(CreateContext)
-     const library = hoset_lib.filter((item) => item.category === "library")[0];
-  
+  const { hoset_lib } = useContext(CreateContext)
+  const library = hoset_lib.filter((item) => item.category === "library")[0];
+
   return (
     <section className='w-full flex flex-col items-center gap-4 justify-center text-center'>
       <h1 className="text-3xl font-semibold text-center">{library.name}</h1>
-      <img src={library.image} alt="" className='w-full lg:w-2/3 rounded-lg'/>
+      <img src={library.image} alt="" className='w-full lg:w-2/3 rounded-lg' />
       <p>{library.description}</p>
 
       <div className='w-full lg:w-2/3 flex flex-col lg:flex-row items-start justify-around'>
-        
+
         <div className='w-full flex flex-col items-start justify-start gap-2'>
           <h1 className="text-2xl font-semibold text-center">Facilities:</h1>
           {
-            library.facilities.map((facility)=>{
+            library.facilities.map((facility) => {
               return <p key={facility.i}>{facility}</p>
             })
           }
@@ -25,16 +25,22 @@ const Library = () => {
         <div className='w-full flex flex-col items-start justify-start gap-2'>
           <h1 className="text-2xl font-semibold text-center">Services:</h1>
           {
-            library.services.map((service)=>{
+            library.services.map((service) => {
               return <p key={service.i}>{service}</p>
             })
           }
         </div>
-        
+
       </div>
 
-      
-      
+      <div className='mt-2 flex flex-col items-start gap-2'>
+        <p >Student capacity : {library.capacity}</p>
+        <h2 className='font-semibold'>Opening Hours:</h2>
+        <p><strong>Weekdays:</strong> {library.opening_hours.weekdays}</p>
+        <p><strong>Weekends:</strong> {library.opening_hours.weekends}</p>
+      </div>
+      <p className='text-red-800'>Location: {library.location}</p>
+
 
     </section>
   )
