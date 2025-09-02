dc654403a39243e055a9df7439d48db8b95f7ca5
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-02 12:59:39 +0600

    update

--- src/pages/Club.jsx
+++ src/pages/Club.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { CreateContext } from '../context/CreateContext'
 
 const Club = () => {
+  const { title } = useParams()
+  const { Club_Lab } = useContext(CreateContext)
+  const club = Club_Lab.find((club) => club.title === title);
+
+  if (!club) return <p className="text-center text-red-500 mt-10">Club not found.</p>;
+
   return (
     <div>
-      
+      <h1 className="text-3xl font-semibold text-center">{club.name}</h1>
+      <div className='w-full flex flex-col items-center justify-center gap-12'>
+       
+      </div>
+
     </div>
   )
 }
