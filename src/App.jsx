923b7929fa5cf13aa417de0642e43e40e357b3ab
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-08 15:06:47 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import Club from "./pages/Club";
 
 const App = () => {
   return (
-    <div className="w-full overflow-x-hidden h-auto text-justify">
+    <div className="w-full overflow-x-hidden h-auto ">
       <Navbar />
       <div className="w-full min-h-screen mt-20 sm:mt-32 p-2 py-6 flex items-center justify-center">
         <Routes>
